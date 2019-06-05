98a44c267d78472bab8920d231c05c41c9fae979
yanlinchengrui <yanlinchengrui@hotmail.com>
2019-06-05 17:47:17 +0000

    urls can be deleted now

--- express_server.js
+++ express_server.js
@@ -16,46 +16,55 @@ const urlDatabase = {
 
 const generateRandomString = () => Math.random().toString(36).substr(2, 6);
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.get("/urls", (req, res) => {
+app.get('/urls', (req, res) => {
   // When sending variables to an EJS template, need to send them inside an object
   let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
-  res.render("urls_show", templateVars);
+  // check if the longURL exists, if not then redirect to /urls
+  templateVars.longURL ? res.render('urls_show', templateVars) : res.redirect('/urls');
 });
 
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   const rand = generateRandomString();
   urlDatabase[rand] = req.body.longURL;
   res.redirect(`/urls/${rand}`);
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
+// catchall route
+app.get('*', (req, res) => res.redirect('/urls'));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
