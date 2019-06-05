a753bc6626188bfd7d8323fa4d7d92f016ef3359
yanlinchengrui <yanlinchengrui@hotmail.com>
2019-06-05 20:58:24 +0000

    implmented login and logout

--- express_server.js
+++ express_server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
-app.set('view engine', 'ejs');
 
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 const PORT = 8080; // default port 8080
 
@@ -30,18 +33,25 @@ app.get('/hello', (req, res) => {
 
 app.get('/urls', (req, res) => {
   // When sending variables to an EJS template, need to send them inside an object
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  let templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"],
   };
   // check if the longURL exists, if not then redirect to /urls
   templateVars.longURL ? res.render('urls_show', templateVars) : res.redirect('/urls');
@@ -70,6 +80,19 @@ app.post('/urls/:shortURL', (req, res) => {
     urlDatabase[short] = long;
   }
   res.redirect(`/urls/${short}`);
+});
+
+app.post('/login', (req, res) => {
+  // set cookies
+  const name = req.body.username;
+  res.cookie('username', name);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  // set cookies
+  res.clearCookie('username');
+  res.redirect('/urls');
 });
 
 // catchall route
